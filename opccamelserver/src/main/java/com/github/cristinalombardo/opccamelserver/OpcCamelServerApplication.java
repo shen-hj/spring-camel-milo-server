db5fe77dfb98b941d80ac31d03d67c2b80c0ea1c
Cristina Lombardo <lombardo.cristina84@gmail.com>
2020-06-02 15:38:14 +0200

    Add server configuration entry point

--- opccamelserver/src/main/java/com/github/cristinalombardo/opccamelserver/OpcCamelServerApplication.java
+++ opccamelserver/src/main/java/com/github/cristinalombardo/opccamelserver/OpcCamelServerApplication.java
@@ -1,19 +1,34 @@
 package com.github.cristinalombardo.opccamelserver;
 
+import org.apache.camel.component.milo.server.MiloServerComponent;
 import org.apache.camel.spring.boot.CamelSpringBootApplicationController;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OpcCamelServerApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext applicationContext = SpringApplication.run(OpcCamelServerApplication.class, args);
-
-		CamelSpringBootApplicationController applicationController =
-				applicationContext.getBean(CamelSpringBootApplicationController.class);
-		applicationController.run();
+		SpringApplication.run(OpcCamelServerApplication.class, args);
 	}
 
+	
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {			
+			MiloServerComponent server = ctx.getBean(MiloServerComponent.class);
+			
+			System.out.println(server.getStatus());
+			
+			//Customize server configuration
+			
+			CamelSpringBootApplicationController applicationController =
+					ctx.getBean(CamelSpringBootApplicationController.class);
+			applicationController.run();
+			
+		};
+	}
 }
